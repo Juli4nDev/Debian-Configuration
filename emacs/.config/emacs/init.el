bd53783a3eee772d89dda6be1199470736faa26e
Julian Acosta <me@juli4n.dev>
2022-11-14 16:02:33 -0500

    feat(git): Add .gitignore

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -1,4 +1,5 @@
 (require 'package)
+
 (add-to-list 'package-archives '("gnu"   . "https://elpa.gnu.org/packages/"))
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
